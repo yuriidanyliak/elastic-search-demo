7e8e23e69b4dcd9866afaa7083a24e2a9cfa21e4
Yuri Danyliak <yuri.danylyak@gmail.com>
2023-08-07 15:58:00 +0300

    Use OpenSearch everywhere.

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -2,7 +2,7 @@ if ENV["BONSAI_URL"].present?
   url = ENV["BONSAI_URL"]
   transport_options = { request: { timeout: 250 } }
   options = { hosts: url, retry_on_failure: true, transport_options: transport_options }
-  Searchkick.client = Elasticsearch::Client.new(options)
+  Searchkick.client = OpenSearch::Client.new(options)
 else
   Searchkick.client = OpenSearch::Client.new(host: ENV['ELASTICSEARCH_URL'])
 end
