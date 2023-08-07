1ccae221eca4191c6bc27e13a566d6958900f98d
Yuri Danyliak <yuri.danylyak@gmail.com>
2023-08-07 15:56:42 +0300

    Add routine code for Heroku startup

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,1 +1,8 @@
-Searchkick.client = OpenSearch::Client.new(host: ENV['ELASTICSEARCH_URL'])
+if ENV["BONSAI_URL"].present?
+  url = ENV["BONSAI_URL"]
+  transport_options = { request: { timeout: 250 } }
+  options = { hosts: url, retry_on_failure: true, transport_options: transport_options }
+  Searchkick.client = Elasticsearch::Client.new(options)
+else
+  Searchkick.client = OpenSearch::Client.new(host: ENV['ELASTICSEARCH_URL'])
+end
